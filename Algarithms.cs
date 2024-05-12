71febc59ce1974601cbe8bac29b89b92083efb1a
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2024-05-12 14:42:46 +0100

    Implemented Binary Search Algorithm To Search For Players In An Array

--- Algarithms.cs
+++ Algarithms.cs
@@ -56,5 +56,67 @@ namespace abstract_data_type_investigation
             }
             return;
         }
+
+        /// <summary>
+        /// Function to make all the given updates to the players array using a binary search algorithm.
+        /// </summary>
+        /// <param name="players">
+        /// The generated array of players.
+        /// </param>
+        /// <param name="changes">
+        /// The generated array of changes.
+        /// </param>
+        public static void makeArrayChangesBinary(this Player[] players, Changes[] changes)
+        {
+            Array.Sort(players, (p1, p2) => p1.getSnowflake().CompareTo(p2.getSnowflake()));
+            for(int i = 0; i < changes.Length; i++)
+            {
+                int index = binarySearch(players, changes[i].getPlayerID());
+                if (index >= 0)
+                {
+                    players[index].updateHealth(changes[i].getHealthChange());
+                }
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Function to perform a binary search on a given players array to locate a spesific player.
+        /// </summary>
+        /// <param name="players">
+        /// The generated array of players.
+        /// </param>
+        /// <param name="targetSnowflake">
+        /// The snowflake of the player being searched for.
+        /// </param>
+        /// <returns>
+        /// Returns the array element index of the player being searched for.
+        /// </returns>
+        public static int binarySearch(Player[] players, long targetSnowflake)
+        {
+            int left = 0;
+            int right = players.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                long snowflake = players[mid].getSnowflake();
+
+                if (snowflake == targetSnowflake)
+                {
+                    return mid; // Player found
+                }
+                else if (snowflake < targetSnowflake)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1; // Player not found
+        }
     }
 }
